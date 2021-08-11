ed39a03de886b0b08dce950457ededa6976fa4e9
AmishMishra11 <amishmishra11@gmail.com>
2021-08-11 22:31:31 +0530

    done

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
+var output = document.querySelector("#output");
+
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function translateText(text) {
+  return serverURL + "?" + "text=" + text;
+}
 
 function clickEvent() {
-  console.log("Clicked");
-  console.log("inpit", txtInput.value);
+  var inputText = txtInput.value;
+  fetch(translateText(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translateText = json.contents.translated;
+      output.innerText = translateText;
+    });
 }
 
 btnTranslate.addEventListener("click", clickEvent);
-
-// // input
-// var name = prompt("enter your name");
-// // procssing
-// var display = "welcome! " + name;
-// //output
-// alert(display);
