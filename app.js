ccc239dbe2fade89ae77df43a5e1c2250ecc92d5
AmishMishra11 <amishmishra11@gmail.com>
2021-08-11 19:27:43 +0530

    taking input and output the value in console

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-// input
-var name = prompt("enter your name");
-// procssing
-var display = "welcome! " + name;
-//output
-alert(display);
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+console.log(txtInput);
+
+function clickEvent() {
+  console.log("Clicked");
+  console.log("inpit", txtInput.value);
+}
+
+btnTranslate.addEventListener("click", clickEvent);
+
+// // input
+// var name = prompt("enter your name");
+// // procssing
+// var display = "welcome! " + name;
+// //output
+// alert(display);
